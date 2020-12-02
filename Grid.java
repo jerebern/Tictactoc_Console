8c8e159e9322de6e6793ad060d3caf44272d28f8
Jeremy Bernard <jere.bern@hotmail.com>
2020-12-01 21:12:49 -0500

    Add game win and cleanup code

--- Grid.java
+++ Grid.java
@@ -1,5 +1,6 @@
 public class Grid {
     private char[][] sqarre;
+    private int playTurn;
     public Grid(){
         //init grid with white space
         this.sqarre = new char [3][3]; 
@@ -27,28 +28,67 @@ public class Grid {
     public boolean Place(String XY, char carToPlace){
         int X = XY.charAt(0) - '0';
         int Y = XY.charAt(1) - '0';
-        if(this.sqarre[X][Y] == ' ' ){
-         this.sqarre[X][Y] = carToPlace;
-            return true;
+        if((X > 0 || X < 3) && (Y > 0 || Y < 3)){
+            if(this.sqarre[X][Y] == ' ' ){
+                this.sqarre[X][Y] = carToPlace;
+                    playTurn ++;
+                   return true;
+               }
+               else{
+                   return false;
+               }
+
         }
-        else{
-            return false;
-        }
+
+        return false;
     }
 
     public boolean CheckifWinner(char PlayerToCheck){
-        int Found = 0;
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
-             if(this.sqarre[i][j] == PlayerToCheck){
-                Found++;
-                if(Found == 2 ) return true;
-             }
-             Found = 0;       // No winner found     
-            }
-           
-        }    
-        //imaginer du code de verification ici
-        return false;
+        //horizontale
+        if(this.sqarre[0][0] == PlayerToCheck && this.sqarre[0][1] == PlayerToCheck && this.sqarre[0][2] == PlayerToCheck){
+            System.out.println("Condition 1");
+            return true;            
+        }
+        else if(this.sqarre[1][0] == PlayerToCheck && this.sqarre[1][1] == PlayerToCheck && this.sqarre[1][2] == PlayerToCheck){
+            System.out.println("Condition 2");
+    
+            return true;
+        }
+        else if(this.sqarre[2][0] == PlayerToCheck && this.sqarre[2][1] == PlayerToCheck && this.sqarre[2][2] == PlayerToCheck){
+            System.out.println("Condition 3");
+    
+            return true;
+        }
+/////////////VERTICAL
+        else if(this.sqarre[0][0] == PlayerToCheck && this.sqarre[1][0] == PlayerToCheck && this.sqarre[2][0] == PlayerToCheck){
+            System.out.println("Condition 4");
+    
+            return true;
+        }
+        else if(this.sqarre[1][0] == PlayerToCheck && this.sqarre[1][1] == PlayerToCheck && this.sqarre[1][2] == PlayerToCheck){
+            System.out.println("Condition 5");
+    
+            return true;
+        }
+        else if(this.sqarre[2][0] == PlayerToCheck && this.sqarre[2][1] == PlayerToCheck && this.sqarre[2][2] == PlayerToCheck){
+            System.out.println("Condition 6");
+    
+            return true;  
+        }
+     ///// HORRIZONTAl   
+        else if(this.sqarre[0][0] == PlayerToCheck && this.sqarre[1][1] == PlayerToCheck && this.sqarre[2][2] == PlayerToCheck){
+            System.out.println("Condition 7");
+    
+            return true;
+        }
+        else if(this.sqarre[2][0] == PlayerToCheck && this.sqarre[1][1] == PlayerToCheck && this.sqarre[0][2] == PlayerToCheck){
+            System.out.println("Condition 8");
+
+            return true;  
+        }
+
+        System.out.println("Condition False");
+
+        return false ;
     }
 }
